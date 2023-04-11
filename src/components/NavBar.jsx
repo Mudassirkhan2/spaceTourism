f22df24abb7a4e0a1b3e0a1a52acf49d878a2ee3
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-04-11 11:51:02 +0530

    fixed image responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from "../assets/shared/logo.svg";
 import {GiCrossedBones} from "react-icons/gi"
 import {RiMenuFoldLine} from "react-icons/ri"
+import { useLocation } from 'react-router-dom';
+
 const NavBar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(true)
- 
+  const location = useLocation();
+  
   const handleToggle = ()=>{
     setShowMediaIcons(!showMediaIcons)
     
@@ -18,20 +21,22 @@ const NavBar = () => {
     <>
       <header className='flex justify-between items-center w-[100%] lg:mt-6 p-8  absolute top-0 left-0 font-BarlowCondensed '>
         <div>
-           <Link >
+           <NavLink >
              <img src={logo} alt="space-tourism" className='w-12 h-12 max-w-none' />
-           </Link>
+           </NavLink>
         </div>
           {
-            showMediaIcons ? <RiMenuFoldLine className=' text-2xl block lg:hidden' onClick={handleToggle}/> : " "
+            showMediaIcons ? <RiMenuFoldLine className=' text-2xl block lg:hidden animate-bounce' onClick={handleToggle}/> : " "
           }
 
-          <div className={`space-x-7 bg-white bg-opacity-5 text-xl  flex flex-col lg:items-center justify-center gap-5 absolute lg:relative lg:flex lg:p-4 lg:rounded-md top-0 right-0 w-9/12 h-screen backdrop-blur z-40 lg:flex-row lg:h-fit lg:w-1/2   ${showMediaIcons ? "hidden" :"block"}`}>
+          <div className={`space-x-7 bg-white  bg-opacity-5 text-xl  flex flex-col lg:items-center justify-center gap-5 absolute  lg:relative  lg:flex lg:p-4 lg:rounded-md top-0 right-0 w-9/12 h-screen backdrop-blur-lg z-40 lg:flex-row lg:h-fit lg:w-1/2   ${showMediaIcons ? "hidden" :"block"}`}>
+            
             <GiCrossedBones className='absolute right-4 top-10 Cross block lg:hidden' onClick={()=>handleToggle()}/>
-            <Link  to="/spaceTourism"  className='tracking-wider ' onClick={handleLinkClick}><span className='pr-2 font-bold'>00</span>HOME</Link>
-            <Link to="/spaceTourism/destination" className='tracking-wider'  onClick={handleLinkClick}><span className='pr-2 font-bold'  onClick={handleLinkClick}>01</span>DESTINATION</Link>
-            <Link to="/spaceTourism/crew" className='tracking-wider'  onClick={handleLinkClick}><span className='pr-2 font-bold'>02</span >CREW</Link>
-            <Link to="/spaceTourism/technology" className='tracking-wider'  onClick={handleLinkClick}><span className='pr-2 font-bold'>03</span>TECHNOLOGY</Link>
+            <NavLink  exact to="/spaceTourism"  className='tracking-wider '  onClick={handleLinkClick}><span className='pr-2 font-bold'>00</span>HOME</NavLink>
+            <NavLink to="/spaceTourism/destination" className='tracking-wider' activeStyle={{ color:'#5754a8' }} onClick={handleLinkClick}><span className='pr-2 font-bold'  onClick={handleLinkClick}>01</span>DESTINATION</NavLink>
+            <NavLink to="/spaceTourism/crew" className='tracking-wider' activeStyle={{ color:'#5754a8' }} onClick={handleLinkClick}><span className='pr-2 font-bold'>02</span >CREW</NavLink>
+            <NavLink to="/spaceTourism/technology" className='tracking-wider' activeStyle={{ color:'#5754a8' }}  onClick={handleLinkClick}><span className='pr-2 font-bold'>03</span>TECHNOLOGY</NavLink>
+
           </div>
       </header>
     </>
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -14,22 +14,20 @@ const Crew = ({data}) => {
       
       {
         <div className='flex flex-col-reverse justify-center lg:flex-row'>
-         
-    
-        <article className='p-4 space-y-4 lg:w-1/3 lg:text-start'>
-            {
-              data.map((persons,index)=> <button key={index} onClick={()=>setValue(index)} className={` pb-2  mx-2   `}>
-                <div className={`p-1 rounded-full w-1 h-1 lg:w-2 lg:h-2 lg:p-2 bg-gray-400 cursor-pointer hover:bg-red-400 ${index===value && "bg-red-400 "}`}></div>
-              </button>)
-            }
-              <h4 className='uppercase font-Bellefair text-gray-400 lg:text-4xl  lg:pt-16'>{role}</h4>
-              <h2 className='text-2xl tracking-wide mx-2 uppercase font-Bellefair lg:text-6xl'>{name}</h2>
-              <p className='text-[#D0D6F9] font-Barlow w-11/12 m-auto lg:m-0 lg:text-xl  lg:pt-6'>{bio}</p>
-          </article>
+            <article className='p-4 space-y-4 lg:w-1/3 lg:text-start'>
+                {
+                  data.map((persons,index)=> <button key={index} onClick={()=>setValue(index)} className={` pb-2  mx-2   `}>
+                    <div className={`p-1 rounded-full w-1 h-1 lg:w-2 lg:h-2 lg:p-2 bg-gray-400 cursor-pointer hover:bg-red-400 ${index===value && "bg-red-400 "}`}></div>
+                  </button>)
+                }
+                  <h4 className='uppercase font-Bellefair text-gray-400 lg:text-4xl  lg:pt-16'>{role}</h4>
+                  <h2 className='text-2xl tracking-wide mx-2 uppercase font-Bellefair lg:text-6xl'>{name}</h2>
+                  <p className='text-[#D0D6F9] font-Barlow w-11/12 m-auto lg:m-0 lg:text-xl  lg:pt-6'>{bio}</p>
+              </article>
 
-          <article className='lg:w-1/2'>
-            <img src={images.png} alt={data[value].name} className='w-1/2  m-auto pt-4 border-b border-[#D0D6F9]' />
-          </article>
+              <article className='lg:w-1/2 lg:self-end'>
+                <img src={images.png} alt={data[value].name} className='w-1/2 h-[350px] m-auto pt-4 border-b border-[#D0D6F9]' />
+              </article>
     </div>
         
       }
